b02c5f4d17d505195e034f9c543765efbbcf1e71
thangnguyenvn98 <thangnguyenvn647@gmail.com>
2023-01-16 20:44:55 -0500

    Responsive design changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import './App.css';
+
 import Navbar from './components/navbar/Navbar'
 import Header from './components/header/Header'
 import FloatingNav from './components/floating-nav/FloatingNav'
